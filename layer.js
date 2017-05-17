b3a98def7cb150d814fd8784b2baacd5eaecf6f5
peppa-peddler <peppasama@gmail.com>
2017-05-17 12:46:48 -0500

    fixing

---
+++ layer.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+      el: '#app2',
+      data: {
+              hello: 'Hello Jorge x!',
+              message: 'You loaded this page one' + new Date(),
+              cond: true
+      }
+})
